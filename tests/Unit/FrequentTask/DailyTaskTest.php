744d76b1fac9307443de1a5e4f975f5fde2a8564
Johannes Wachter <johannes.wachter@massiveart.at>
2015-12-13 10:56:24 +0100

    added delta to make tests reliable

--- tests/Unit/FrequentTask/DailyTaskTest.php
+++ tests/Unit/FrequentTask/DailyTaskTest.php
@@ -2,6 +2,7 @@
 
 namespace Unit\FrequentTask;
 
+use Prophecy\Argument;
 use Task\FrequentTask\DailyTask;
 use Task\Scheduler;
 use Task\TaskBuilder;
@@ -14,14 +15,14 @@ class DailyTaskTest extends \PHPUnit_Framework_TestCase
         return [
             [new \DateTime('2 days ago'), new \DateTime('1 day ago')],
             [new \DateTime('1 day ago'), new \DateTime('+2 days'), new \DateTime('+1 day')],
-            [new \DateTime('1 day ago'), new \DateTime('+1 days'), new \DateTime('+1 day')],
+            [new \DateTime('1 day ago'), new \DateTime('+25 hours'), new \DateTime('+1 day')],
         ];
     }
 
     /**
      * @dataProvider scheduleNextProvider
      */
-    public function testScheduleNext(\DateTime $start, \DateTime $end, $scheduledExecutionDate = null)
+    public function testScheduleNext(\DateTime $start, \DateTime $end, \DateTime $scheduledExecutionDate = null)
     {
 
         $scheduler = $this->prophesize(Scheduler::class);
@@ -30,8 +31,15 @@ class DailyTaskTest extends \PHPUnit_Framework_TestCase
             $scheduler->createTask('test-task', 'test-task: workload')->willReturn($taskBuilder->reveal());
 
             $taskBuilder->daily($start, $end)->shouldBeCalledTimes(1)->willReturn($taskBuilder->reveal());
-            $taskBuilder->setExecutionDate($scheduledExecutionDate)
-                ->shouldBeCalledTimes(1)->willReturn($taskBuilder->reveal());
+            $taskBuilder->setExecutionDate(
+                Argument::that(
+                    function (\DateTime $dateTime) use ($scheduledExecutionDate) {
+                        $this->assertEquals($scheduledExecutionDate->getTimestamp(), $dateTime->getTimestamp(), '', 2);
+
+                        return true;
+                    }
+                )
+            )->shouldBeCalledTimes(1)->willReturn($taskBuilder->reveal());
             $taskBuilder->schedule()->shouldBeCalledTimes(1)->willReturn($taskBuilder->reveal());
         }
 
