185487438604e5498937a32d36c1be8e0e6ede47
Johannes Wachter <johannes.wachter@massiveart.at>
2016-01-13 07:52:23 +0100

    fixed testcases

--- tests/Unit/FrequentTask/DailyTaskTest.php
+++ tests/Unit/FrequentTask/DailyTaskTest.php
@@ -35,15 +35,19 @@ class DailyTaskTest extends \PHPUnit_Framework_TestCase
     /**
      * @dataProvider scheduleNextProvider
      */
-    public function testScheduleNext(\DateTime $start, \DateTime $end, \DateTime $scheduledExecutionDate = null)
-    {
-
+    public function testScheduleNext(
+        \DateTime $start,
+        \DateTime $end,
+        \DateTime $scheduledExecutionDate = null,
+        $key = null
+    ) {
         $scheduler = $this->prophesize(Scheduler::class);
         if ($scheduledExecutionDate !== null) {
             $taskBuilder = $this->prophesize(TaskBuilder::class);
             $scheduler->createTask('test-task', 'test-task: workload')->willReturn($taskBuilder->reveal());
 
             $taskBuilder->daily($start, $end)->shouldBeCalledTimes(1)->willReturn($taskBuilder->reveal());
+            $taskBuilder->setKey($key)->shouldBeCalledTimes(1)->willReturn($taskBuilder->reveal());
             $taskBuilder->setExecutionDate(
                 Argument::that(
                     function (\DateTime $dateTime) use ($scheduledExecutionDate) {
@@ -59,6 +63,8 @@ class DailyTaskTest extends \PHPUnit_Framework_TestCase
         $task = $this->prophesize(TaskInterface::class);
         $task->getTaskName()->willReturn('test-task');
         $task->getWorkload()->willReturn('test-task: workload');
+        $task->getExecutionDate()->willReturn(new \DateTime());
+        $task->getKey()->willReturn($key);
 
         $task = new DailyTask($task->reveal(), $start, $end);
 
